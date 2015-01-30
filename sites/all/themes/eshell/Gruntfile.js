bdeec22dfb60ebb7c7bcea35d64983b7acf6cfed
Eric Shell <ericjshell@gmail.com>
2015-01-30 12:03:27 -0800

    Added Grunt.js to compile multiple LESS files into one production CSS file.

---
+++ sites/all/themes/eshell/Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
+     concat: {
+        main: {
+           src: [
+              'less/src/base.less',
+              'less/src/regions/*.less',
+              'less/src/media-queries/*.less',
+              'css/normalize.css',
+           ],
+           dest: 'less/production.less'
+        }
+     },
+     less: {
+        compile: {
+           files: { 
+              // format "target" : "source"
+              "css/production.css": "less/production.less"
+           }
+        }
+     },
+     /* 
+     csscomb: {
+        main: {
+            files: {
+                'production.css': ['clean.css'],
+            }
+        }
+    },
+    */
+     watch: {
+        files: [
+          'less/src/media-queries/*.less',
+          'less/src/regions/*.less',
+        ],
+        tasks: ['concat', 'less'] 
+     }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-assemble-less');
+  //grunt.loadNpmTasks('grunt-csscomb');
+  
+  grunt.registerTask('default', ['concat', 'less']);
+
+  //grunt.registerTask('clean', ['csscomb']);
+};
