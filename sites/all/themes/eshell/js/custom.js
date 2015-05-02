8d67f8ea8d3324442cd936a0972b61cc2a26c61e
Eric Shell <ericjshell@gmail.com>
2015-05-02 13:01:33 -0700

    Configured and added initial styles for flippy module.

--- sites/all/themes/eshell/js/custom.js
+++ sites/all/themes/eshell/js/custom.js
@@ -1,7 +1,22 @@
 jQuery(document).ready(function () {
+   // removes anchor tag from images in body text
+   jQuery('.field-name-body p img').each(function() {
+      jQuery(this).unwrap();
+   }); 
 
-    jQuery('.field-name-body p img').each(function() {
-        jQuery(this).unwrap();
-    }); 
+   jQuery(document).scroll(function() {
+      // controls visbility of article pager
+      var article_pager = false;
+      // calculates height of page elements
+      var current_height = jQuery(document).scrollTop();
+      var content_height = jQuery('#main').position()["top"];
+
+      if(current_height > content_height){
+         article_pager = true;
+      }
+
+      jQuery('.flippy li a').toggleClass('pager-visible', article_pager);
+   });
 
 });
+
