8e0332aef85ad64611c586c407552b3a8fc14909
Eric Shell <ericjshell@gmail.com>
2015-05-06 11:22:07 -0700

    Cleaned up excessive code in all page templates.

--- sites/all/themes/eshell/templates/page--node--1.tpl.php
+++ sites/all/themes/eshell/templates/page--node--1.tpl.php
@@ -2,16 +2,12 @@
 
 <section id="site">
 
-  <!-- header start -->
   <section id="header">
 
-    <!-- wrapper start -->
     <section class="wrapper">
 
-      <!-- content start -->
       <section class="content">
 
-        <!-- logo start -->
         <div id="logo">
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><?php print t('Eric Shell'); ?></a>
         </div>
@@ -20,29 +16,26 @@
         <?php print render($page['header']); ?>
 
       </section>
-      <!-- content end -->
+      <!-- header content end -->
 
     </section>
-    <!-- wrapper end -->
+    <!-- header wrapper end -->
 
   </section>
   <!-- header end -->
 
-  <!-- main container start -->
   <section id="main">
 
     <?php if ($messages): ?>
       <div id="messages"><?php print $messages; ?></div>
     <?php endif; ?>
 
-    <!-- content start -->
+    <?php if ($tabs = render($tabs)): ?>
+      <div class="tabs"><?php print $tabs; ?></div>
+    <?php endif; ?>
+
     <section class="content">
 
-      <?php if ($tabs = render($tabs)): ?>
-        <div class="tabs"><?php print $tabs; ?></div>
-      <?php endif; ?>
-
-      <!-- content start -->
 	    <section class="sidebar">
 
         <div id="profile-photo">
@@ -63,22 +56,15 @@
 	    </section>
 	    <!-- sidebar end -->
     
-      <?php print render($page['content']); ?>
-      <?php print render($title_suffix); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-
-      <?php print render($page['feed']); ?>   
+      <?php print render($page['content']); ?> 
 
     </section>
-    <!-- content end -->
+    <!-- main content end -->
 
   </section>
   <!-- main container end -->
 
 </section>
-<!-- Site Content Ends -->
+<!-- site end -->
 
 <?php print render($page['bottom']); ?>
