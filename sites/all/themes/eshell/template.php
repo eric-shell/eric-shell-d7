da11badb1be03ba63992228bfcf8ab88560311aa
eric-shell <ericjshell@gmail.com>
2015-07-09 20:49:42 -0700

    Simplifying php template file

--- sites/all/themes/eshell/template.php
+++ sites/all/themes/eshell/template.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
-* adjusted functionality and information on content types
-* implements template_preprocess_node()
-* 
-* @param includes node, view_mode and page variables
-*/
 function eshell_preprocess_node(&$vars) {
    $content_type = $vars['type'];
    $view_mode = $vars['view_mode'];
@@ -16,11 +10,3 @@ function eshell_preprocess_node(&$vars) {
    // Displays date on pages with simpler format
    $vars['submitted'] = t('@date', array('@date' => date("M jS", $vars['created'])));
 }
-
-/* search bar placeholder text
-function eshell_form_search_block_form_alter(&$form, &$form_state, $form_id) {
-  if (isset($form['search_block_form'])) {
-    $form['search_block_form']['#title'] = t('Enter search terms here');
-  }
-}
-*/
