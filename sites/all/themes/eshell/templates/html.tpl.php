d24b1e56130913117b07f3917e3ffa46ac20cb3f
eric-shell <ericjshell@gmail.com>
2015-07-11 10:52:53 -0700

    Replacing Google Analytics modules with script in header template

--- sites/all/themes/eshell/templates/html.tpl.php
+++ sites/all/themes/eshell/templates/html.tpl.php
@@ -84,6 +84,14 @@
   <!--<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,900,400italic,900italic' rel='stylesheet' type='text/css'>-->
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-58097897-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
