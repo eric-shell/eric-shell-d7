e47968c58c2eec37d37d636b38547f001589f8a1
eric-shell <ericjshell@gmail.com>
2015-07-11 11:24:14 -0700

    Fixing misspelling in comment of theme suggestion hook

--- sites/all/themes/eshell/template.php
+++ sites/all/themes/eshell/template.php
@@ -4,7 +4,7 @@ function eshell_preprocess_node(&$vars) {
    $content_type = $vars['type'];
    $view_mode = $vars['view_mode'];
 
-   // Creates page tempaltes for each view mode of a content type
+   // Creates page templates for each view mode of a content type
    $vars['theme_hook_suggestions'][] = 'node__' . $content_type . '__' . $view_mode;
 
    // Displays date on pages with simpler format
