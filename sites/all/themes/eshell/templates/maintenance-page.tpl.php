b0ef651644df220f6e0298b824b423c100cd103f
eric-shell <ericjshell@gmail.com>
2015-07-21 22:13:43 -0700

    Cleaning up maintenance page template

--- sites/all/themes/eshell/templates/maintenance-page.tpl.php
+++ sites/all/themes/eshell/templates/maintenance-page.tpl.php
@@ -7,56 +7,40 @@
  * @see https://drupal.org/node/1728174
  */
 ?><!DOCTYPE html>
-<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes; ?>><!--<![endif]-->
-
 <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-
-  <?php if ($default_mobile_metatags): ?>
-    <meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
-  <?php endif; ?>
+  <meta name="MobileOptimized" content="width">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="viewport" content="width=device-width">
   <meta http-equiv="cleartype" content="on">
-
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <?php if ($add_respond_js): ?>
-    <!--[if lt IE 9]>
-    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5-respond.js"></script>
-    <![endif]-->
-  <?php elseif ($add_html5_shim): ?>
-    <!--[if lt IE 9]>
-    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5.js"></script>
-    <![endif]-->
-  <?php endif; ?>
 </head>
+
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 
-<section id="site">
+  <section id="site">
 
-  <?php include "header.tpl.php"; ?>
+    <?php include "header.tpl.php"; ?>
 
-  <section id="main">
+    <section id="main">
 
-    <section class="content">
+      <section class="content">
 
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
+        <?php if ($title): ?>
+          <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
 
-      <?php print render($page['content']); ?>
+        <?php print render($page['content']); ?>
+
+      </section>
+      <!-- main content end -->
 
     </section>
-    <!-- main content end -->
+    <!-- main container end -->
 
   </section>
-  <!-- main container end -->
+  <!-- site end -->
 
-</section>
-<!-- site end -->
+</body>
