ee6c0e83c6ae686330dafa0332d40fdebc9da929
eric-shell <ericjshell@gmail.com>
2015-07-22 22:00:01 -0700

    Adding template suggestions for teasers

--- sites/all/themes/eshell/template.php
+++ sites/all/themes/eshell/template.php
@@ -1,12 +1,18 @@
 <?php
 
 function eshell_preprocess_node(&$vars) {
-   $content_type = $vars['type'];
-   $view_mode = $vars['view_mode'];
+  $content_type = $vars['type'];
+  $view_mode = $vars['view_mode'];
 
-   // Creates page templates for each view mode of a content type
-   $vars['theme_hook_suggestions'][] = 'node__' . $content_type . '__' . $view_mode;
+  // Creates page templates for each view mode of a content type
+  $vars['theme_hook_suggestions'][] = 'node__' . $content_type . '__' . $view_mode;
 
-   // Displays date on pages with simpler format
-   $vars['submitted'] = t('@date', array('@date' => date("M jS", $vars['created'])));
+  // Creates page template suggestions for teasers
+  if($vars['view_mode'] == 'teaser') {
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__teaser';   
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->nid . '__teaser';
+  }
+
+  // Displays date on pages with simpler format
+  $vars['submitted'] = t('@date', array('@date' => date("M jS", $vars['created'])));
 }
