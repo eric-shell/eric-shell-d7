99e4400688805262c684bfaaf1e88ca0d606cbd9
eric-shell <ericjshell@gmail.com>
2015-07-29 00:21:05 -0700

    Creating initial styles for photo teasers

---
+++ sites/all/themes/eshell/templates/node--photo--teaser.tpl.php
@@ -0,0 +1,14 @@
+<article class="photo-teaser node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="photo-container">
+
+    <div class="photo-image">
+      <?php print render($content['field_image']); ?>
+    </div>
+
+    <?php if (!$page && $title): ?>
+      <a class="photo-title" href="<?php print $node_url; ?>"><?php print $title; ?></a>
+    <?php endif; ?>
+
+  </div>
+</article>
+<!-- photo teaser end -->
