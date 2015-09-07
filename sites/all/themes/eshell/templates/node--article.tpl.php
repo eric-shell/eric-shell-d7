1dfebbce4838b8befbe717a0abca0155ab833c04
eric-shell <ericjshell@gmail.com>
2015-09-07 15:14:18 -0700

    Theming all major components on article nodes

---
+++ sites/all/themes/eshell/templates/node--article.tpl.php
@@ -0,0 +1,26 @@
+<article class="article-full node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
+  <div id="node-header">
+    <div class="container">
+      <h1<?php print $title_attributes; ?>>
+        <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+      </h1>
+      <div class="submitted">
+        <?php print $submitted; ?>
+      </div>
+    </div>
+  </div>
+
+  <div id="node-content">
+    <div class="container">
+      <?php print render($content['field_video']); ?>
+      <?php print render($content['body']); ?>
+    </div>
+  </div>
+
+  <div id="node-filters">
+    <?php print render($content['field_media_type']); ?>
+    <?php print render($content['field_tags']); ?>
+  </div>
+</article>
+
+<?php print render($content["flippy_pager"]); ?>
