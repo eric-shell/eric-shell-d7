78b1ab642323d6bb3d270984b2906c3dd5acfe9f
eric-shell <ericjshell@gmail.com>
2015-09-10 21:14:50 -0700

    Adding full and container mixins

--- sites/all/themes/eshell/templates/navigation.tpl.php
+++ sites/all/themes/eshell/templates/navigation.tpl.php
@@ -1,4 +1,6 @@
-<nav id="nav" role="navigation">
-  <a href="<?php print $front_page; ?>" title="<?php print t('Eric Shell'); ?>" rel="home" id="home"><img src="/sites/all/themes/eshell/assets/images/logo.svg" title="<?php print t('Eric Shell'); ?>" alt="<?php print t('Eric Shell'); ?>"></a>
-  <?php print render($page['main_menu']); ?>
+<nav id="nav" class="full" role="navigation">
+  <div class="container">
+    <a href="<?php print $front_page; ?>" title="<?php print t('Eric Shell'); ?>" rel="home" id="home"><img src="/sites/all/themes/eshell/assets/images/logo.svg" title="<?php print t('Eric Shell'); ?>" alt="<?php print t('Eric Shell'); ?>"></a>
+    <?php print render($page['main_menu']); ?>
+  </div>
 </nav>
