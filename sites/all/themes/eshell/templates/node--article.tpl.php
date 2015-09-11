0c3af264f9702cccd38207b4f2d1f8f42f652882
eric-shell <ericjshell@gmail.com>
2015-09-10 22:05:20 -0700

    Converted article nodes to use new mixins

--- sites/all/themes/eshell/templates/node--article.tpl.php
+++ sites/all/themes/eshell/templates/node--article.tpl.php
@@ -1,12 +1,12 @@
 <article class="article-full node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
-  <div id="node-video">
+  <div id="node-video" class="full">
     <div class="container">
       <?php print render($content['field_video']); ?>
     </div>
   </div>
 
-  <div id="node-header">
-    <div class="container">
+  <div id="node-header" class="full">
+    <div class="container inner">
       <h1<?php print $title_attributes; ?>>
         <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
       </h1>
@@ -16,15 +16,17 @@
     </div>
   </div>
 
-  <div id="node-content">
+  <div id="node-content" class="full">
     <div class="container">
       <?php print render($content['body']); ?>
     </div>
   </div>
 
-  <div id="node-filters">
-    <?php print render($content['field_media_type']); ?>
-    <?php print render($content['field_tags']); ?>
+  <div id="node-filters" class="full">
+    <div class="container center">
+      <?php print render($content['field_media_type']); ?>
+      <?php print render($content['field_tags']); ?>
+    </div>
   </div>
 </article>
 
