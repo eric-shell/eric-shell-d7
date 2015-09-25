d796f1854882413b8c5dc8005ea5da1e47996810
eric-shell <ericjshell@gmail.com>
2015-09-24 22:25:03 -0700

    Refactoring Article teasers for large screen sizes

--- sites/all/themes/eshell/templates/node--article--teaser.tpl.php
+++ sites/all/themes/eshell/templates/node--article--teaser.tpl.php
@@ -1,47 +1,32 @@
 <article class="article-teaser node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="article-container">
 
     <?php if($zebra == 'odd'): ?>
 
-    <div class="article-image">
-      <?php print render($content['field_image']); ?>
-      <?php print render($content['links']); ?>
-    </div>
+      <div class="article-image article-section">
+        <?php print render($content['field_image']); ?>
+        <?php print render($content['links']); ?>
+      </div>
 
-    <div class="article-content">
-      <?php if (!$page && $title): ?>
+      <div class="article-content article-section">
         <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-
-      <?php print render($content['body']); ?>
-
-      <div class="node-filters">
+        <?php print render($content['body']); ?>
         <?php print render($content['field_tags']); ?>
       </div>
-    </div>
-
-    <?php endif; ?>
-
-    <?php if($zebra == 'even'): ?>
-
-    <div class="article-content">
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-
-      <?php print render($content['body']); ?>
-
-      <div class="node-filters">
-        <?php print render($content['field_tags']); ?>
-      </div>
-    </div>
-
-    <div class="article-image">
-      <?php print render($content['field_image']); ?>
-      <?php print render($content['links']); ?>
-    </div>
     
     <?php endif; ?>
 
-  </div>
+    <?php if($zebra == 'even'): ?>
+      <div class="article-content article-section">
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+        <?php print render($content['body']); ?>
+        <?php print render($content['field_tags']); ?>
+      </div>
+
+      <div class="article-image article-section">
+        <?php print render($content['field_image']); ?>
+        <?php print render($content['links']); ?>
+      </div>
+    
+    <?php endif; ?>
+
 </article>
