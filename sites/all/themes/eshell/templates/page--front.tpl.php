2ffeb0c026868f51e7343fd7843a593c73a0723b
eric-shell <ericjshell@gmail.com>
2015-09-25 13:25:17 -0700

    Cleaning up header markup and using more accurate naming conventions for classes

--- sites/all/themes/eshell/templates/page--front.tpl.php
+++ sites/all/themes/eshell/templates/page--front.tpl.php
@@ -9,17 +9,13 @@
   <header id="header" role="header">
     <div id="header-container">
 
-      <div id="profile">
-        <div id="headshot">
-          <img src="sites/all/themes/eshell/assets/images/profile-alt.jpg">
-        </div>
+      <div id="header-image">
+        <img id="headshot" src="sites/all/themes/eshell/assets/images/profile-alt.jpg">
         <a href="<?php print $front_page; ?>" title="<?php print t('Eric Shell'); ?>" rel="home" id="logo"><img src="/sites/all/themes/eshell/assets/images/logo.svg" title="<?php print t('Eric Shell'); ?>" alt="<?php print t('Eric Shell'); ?>"></a>
       </div>
 
-      <div id="intro">
-        <div class="content">
-          <?php print render($node->body['und']['0']['value']); ?>
-        </div>
+      <div id="header-content">
+        <?php print render($node->body['und']['0']['value']); ?>
         <a href="/contact" class="button">Learn more</a>
       </div>
       
