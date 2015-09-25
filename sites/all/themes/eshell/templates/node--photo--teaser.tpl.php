0d77c9a12f879cc10d65e906a02128e10a49d2df
eric-shell <ericjshell@gmail.com>
2015-09-24 21:09:56 -0700

    Refactoring photo teaser markup

--- sites/all/themes/eshell/templates/node--photo--teaser.tpl.php
+++ sites/all/themes/eshell/templates/node--photo--teaser.tpl.php
@@ -1,13 +1,11 @@
-<article class="photo-teaser node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="photo-container">
-
-    <div class="photo-image">
-      <?php print render($content['field_image']); ?>
-    </div>
-
-    <?php if (!$page && $title): ?>
-      <a class="photo-title" href="<?php print $node_url; ?>">View <?php print $title; ?></a>
-    <?php endif; ?>
-
+<article class="photo-teaser node-<?php print $node->nid; ?> <?php print $classes; ?>"<?php print $attributes; ?>>
+  
+  <div class="photo-image">
+    <?php print render($content['field_image']); ?>
   </div>
+
+  <?php if (!$page && $title): ?>
+    <a class="photo-title" href="<?php print $node_url; ?>">View <?php print $title; ?></a>
+  <?php endif; ?>
+  
 </article>
