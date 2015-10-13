8b1a6e3097388a590223a226b9a3b60f2bd8ce79
eric-shell <ericjshell@gmail.com>
2015-10-12 21:52:20 -0700

    Began working on 404 page and fixed contact page background color

--- sites/all/themes/eshell/template.php
+++ sites/all/themes/eshell/template.php
@@ -13,6 +13,14 @@ function eshell_preprocess_node(&$vars) {
   $vars['submitted'] = t('@date', array('@date' => date("F jS, Y", $vars['created'])));
 }
 
+// Creating 404 page template
+function eshell_preprocess_page(&$vars) {
+  $header = drupal_get_http_header("status");
+  if ($header == "404 Not Found") {
+    $vars['theme_hook_suggestions'][] = 'page__404';
+  }
+}
+
 // User login placeholders
 function eshell_form_alter( &$form, &$form_state, $form_id ) {
 if ( TRUE === in_array( $form_id, array( 'user_login', 'user_login_block') ) )
---
+++ sites/all/themes/eshell/templates/page--404.tpl.php
@@ -0,0 +1,9 @@
+<section id="site">
+
+  <main id="main" role="content">
+    <p>Bummer dude.  That page doesn't exist.</p>
+    <img src="sites/all/themes/eshell/assets/images/morty.gif">
+  </main>
+
+</section>
+<!-- site end -->
